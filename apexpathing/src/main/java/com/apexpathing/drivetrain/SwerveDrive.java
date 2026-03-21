d9b0fd3fb013aa3ad2de54f15c5c209f98b4a940
mxtmx <142958831+mxtmx@users.noreply.github.com>
2026-03-21 12:29:45 -0700

    Restructure repository as publishable Apex Pathing JitPack library (#15)
    * Restructure repository as publishable Apex Pathing JitPack library
    
    Converts the repo from a messy FTC app into a clean Android library dependency under com.apexpathing. Integrates collaborator drivetrain commits (Drivetrain, MecanumDrive, TankDrive, MecanumConstants), fixes a NPE bug in MecanumConstants, sets up maven-publish for JitPack, and updates LICENSE to MIT.
    
    * feat: add FtcRobotController and TeamCode modules for on-robot testing
    
    * Clean up merge
    
    * Clean up merge again

---
+++ apexpathing/src/main/java/com/apexpathing/drivetrain/SwerveDrive.java
@@ -0,0 +1,102 @@
+package com.apexpathing.drivetrain;
+
+import com.qualcomm.robotcore.hardware.AnalogInput;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import com.apexpathing.geometry.Pose2d;
+import com.apexpathing.geometry.Vector2d;
+import com.apexpathing.hardware.MotorEx;
+import com.apexpathing.kinematics.ChassisSpeeds;
+import com.apexpathing.kinematics.SwerveKinematics;
+import com.apexpathing.kinematics.SwerveModuleState;
+import com.apexpathing.localization.Localizer;
+import com.apexpathing.follower.HolonomicTrajectoryFollower;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * SwerveDrive implementation following the 5-part structure.
+ */
+public class SwerveDrive extends CustomDrive {
+    private final SwerveModule[] modules = new SwerveModule[4];
+    private final SwerveKinematics kinematics;
+
+    public SwerveDrive(HardwareMap hardwareMap, Localizer localizer, Vector2d[] moduleOffsets) {
+        this.localizer = localizer;
+        this.kinematics = new SwerveKinematics(moduleOffsets);
+        this.controller = new HolonomicTrajectoryFollower(kinematics);
+
+        String[] moduleNames = {"FL", "FR", "BL", "BR"};
+        for (int i = 0; i < 4; i++) {
+            MotorEx driveMotor = new MotorEx(hardwareMap.get(DcMotorEx.class, moduleNames[i] + "Drive"));
+            Servo turnServo = hardwareMap.get(Servo.class, moduleNames[i] + "Turn");
+            AnalogInput absoluteEncoder = hardwareMap.get(AnalogInput.class, moduleNames[i] + "Encoder");
+
+            modules[i] = new SwerveModule(driveMotor, turnServo, absoluteEncoder);
+        }
+    }
+
+    private Pose2d currentPose = new Pose2d(0, 0, 0);
+    private Pose2d targetVelocity = new Pose2d(0, 0, 0);
+
+    @Override
+    public void setDrivePowers(Pose2d drivePowers) {
+        this.targetVelocity = drivePowers;
+        ChassisSpeeds chassisSpeeds = new ChassisSpeeds(drivePowers.x, drivePowers.y, drivePowers.heading);
+        SwerveModuleState[] states = kinematics.calculate(chassisSpeeds);
+
+        for (int i = 0; i < 4; i++) {
+            modules[i].setDesiredState(states[i].speed, states[i].angle);
+        }
+    }
+
+    @Override
+    public void update() {
+        localizer.update();
+        currentPose = localizer.getPose();
+
+    }
+
+    public List<Double> getModulePositions() {
+        List<Double> positions = new ArrayList<>();
+        for (SwerveModule module : modules) {
+            positions.add(module.getWheelPosition());
+        }
+        return positions;
+    }
+
+    public List<Double> getModuleAngles() {
+        List<Double> angles = new ArrayList<>();
+        for (SwerveModule module : modules) {
+            angles.add(module.getModuleAngle());
+        }
+        return angles;
+    }
+
+    public double[] getTargetAngles() {
+        double[] targets = new double[4];
+        for (int i = 0; i < 4; i++) {
+            targets[i] = modules[i].getLastPosition() * 2.0 * Math.PI;
+        }
+        return targets;
+    }
+
+    public double[] getActualAngles() {
+        double[] actuals = new double[4];
+        for (int i = 0; i < 4; i++) {
+            actuals[i] = modules[i].getModuleAngle();
+        }
+        return actuals;
+    }
+
+    public Pose2d getCurrentPose() {
+        return currentPose;
+    }
+
+    public Pose2d getTargetVelocity() {
+        return targetVelocity;
+    }
+}
