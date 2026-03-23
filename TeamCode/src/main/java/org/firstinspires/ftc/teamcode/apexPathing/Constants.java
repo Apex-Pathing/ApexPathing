bea6ea235ddc46d84043ce7c9bf133601dc50a64
athGurnani <atharvgurnani@gmail.com>
2026-03-23 18:09:41 -0500

    Remove Panels from Constants file.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/apexPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/apexPathing/Constants.java
@@ -1,12 +1,9 @@
 package org.firstinspires.ftc.teamcode.apexPathing;
 
 import com.apexpathing.drivetrain.MecanumConstants;
-import com.apexpathing.drivetrain.MecanumDrive;
-import com.apexpathing.localization.PinpointLocalizer;
-import com.bylazar.configurables.annotations.Configurable;
+\import com.apexpathing.localization.PinpointLocalizer;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
-@Configurable
 public class Constants {
     public static PinpointLocalizer localizer = null;
     public static MecanumConstants dtConstants = null;
